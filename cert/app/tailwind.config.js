5d1d263121364fc957b519085d0c59eb701e68ba
ZephyrVictor <empire258700@gmail.com>
2024-11-29 22:19:50 +0800

    重构了cert的前端 加了taildwindcss

---
+++ cert/app/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html", // 确保 TailwindCSS 处理 Flask 模板
+    "./static/**/*.js",      // 如果有静态的 JavaScript 文件
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
